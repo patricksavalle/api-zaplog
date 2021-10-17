b85790e32a034fe3ec2385308246d1a3e7f9d0ce
Patrick Savalle <patrick@patricksavalle.com>
2021-10-18 00:44:01 +0200

    Bug fixes.

--- Methods.php
+++ Methods.php
@@ -113,7 +113,7 @@ namespace Zaplog {
                 $metadata["url"],
                 $metadata["title"],
                 $metadata["description"] ?? "",
-                $metadata["image"] ?? "https://cdn.pixabay.com/photo/2018/06/24/08/01/dark-background-3494082_1280.jpg",
+                $metadata["image"] ?? Ini::get("default_post_image"),
                 $metadata["keywords"] ?? []
             );
         }
