e22f31c2ccdbbdef3ac699b24121833de65e1461
Patrick Savalle <patrick@patricksavalle.com>
2021-10-25 17:45:59 +0200

    Different strategy. If no working version is found, just return original.

--- Library/ParsedownProcessor.php
+++ Library/ParsedownProcessor.php
@@ -8,13 +8,16 @@ namespace Zaplog\Library {
 
     class ParsedownProcessor
     {
+        // Used in the Parsedown parser to prcoess XHTML elements before they're outputted
         function __invoke($element): array
         {
             static $redirector = null;
             if ($redirector === null) $redirector = Ini::get("broken_link_redirector");
+            // add the redirector service to href's
             if (isset($element['attributes']['href'])) {
                 $element['attributes']['href'] = $redirector . $element['attributes']['href'];
             }
+            // add target= to links
             if (strcasecmp($element['name'], 'a') === 0) {
                 $element['attributes']['target'] = '_blank';
             }
