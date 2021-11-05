cc9d50885cbf372fa1e1fa8e088d60de74e558c2
Patrick Savalle <patrick@patricksavalle.com>
2021-11-05 09:59:11 +0100

    More robust getMimetype()

--- Plugins/MetadataParser.php
+++ Plugins/MetadataParser.php
@@ -27,11 +27,16 @@ namespace Zaplog\Plugins {
                 // HEAD request to check mimetype
                 curl_setopt($curl, CURLOPT_NOBODY, true);
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 OPR/78.0.4093.112"); // some feeds require a user agent
                 curl_exec($curl);
                 if (($errno = curl_errno($curl)) !== 0) {
                     throw new Exception(curl_strerror($errno), 400);
                 }
-                preg_match("/([^;]+)/", curl_getinfo($curl, CURLINFO_CONTENT_TYPE), $matches);
+                $contenttype = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+                if ($contenttype===false) {
+                    throw new Exception("CURLINFO_CONTENT_TYPE error");
+                }
+                preg_match("/([^;]+)/", $contenttype, $matches);
                 return $matches[1];
             } finally {
                 curl_close($curl);
