f41e3b13f68c6dc4fb944709543eab5d316916a2
Patrick Savalle <patrick@patricksavalle.com>
2021-11-08 15:36:45 +0100

    For some reason local variables in stored procedure don't work. Work around that.

--- datamodel.sql
+++ datamodel.sql
@@ -261,14 +261,12 @@ CREATE VIEW frontpage AS
 DELIMITER //
 CREATE EVENT select_frontpage ON SCHEDULE EVERY 60 MINUTE DO
     BEGIN
-        DECLARE language CHAR(2);
-        SET @language = (SELECT language FROM channels WHERE id=1);
         CREATE TABLE frontpage_new
         SELECT DISTINCT links.* FROM links
-            WHERE published=TRUE
-              AND @language IS NULL OR @language=language
-              AND createdatetime<SUBDATE(CURRENT_TIMESTAMP, INTERVAL 3 HOUR)
-            ORDER BY (score / GREATEST(9, POW(TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, createdatetime),2))) DESC LIMIT 18;
+        WHERE published=TRUE
+          AND language=IFNULL((SELECT language FROM channels WHERE id=1),language)
+          AND createdatetime<SUBDATE(CURRENT_TIMESTAMP, INTERVAL 3 HOUR)
+        ORDER BY (score / GREATEST(9, POW(TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, createdatetime),2))) DESC LIMIT 18;
         -- atomic swap
         RENAME TABLE frontpage_current TO frontpage_old, frontpage_new TO frontpage_current;
         DROP TABLE frontpage_old;
--- datamodel_change.sql
+++ datamodel_change.sql
@@ -1,2 +1,17 @@
-DROP PROCEDURE IF EXISTS zaplog.select_discussion;
-SET GLOBAL TRANSACTION ISOLATION LEVEL REPEATABLE READ;
+USE zaplog;
+UPDATE channels SET language='nl' WHERE id=1;
+DROP EVENT IF EXISTS zaplog.select_frontpage;
+DELIMITER //
+CREATE EVENT select_frontpage ON SCHEDULE EVERY 60 MINUTE DO
+    BEGIN
+        CREATE TABLE frontpage_new
+        SELECT DISTINCT links.* FROM links
+        WHERE published=TRUE
+          AND language=IFNULL((SELECT language FROM channels WHERE id=1),language)
+          AND createdatetime<SUBDATE(CURRENT_TIMESTAMP, INTERVAL 3 HOUR)
+        ORDER BY (score / GREATEST(9, POW(TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, createdatetime),2))) DESC LIMIT 18;
+        -- atomic swap
+        RENAME TABLE frontpage_current TO frontpage_old, frontpage_new TO frontpage_current;
+        DROP TABLE frontpage_old;
+    END //
+DELIMITER ;
