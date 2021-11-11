cf1bf29f7cbb8e73aef2bd0f50cb9c9b84dbfe5b
Patrick Savalle <patrick@patricksavalle.com>
2021-11-11 20:58:00 +0100

    Dankjewel Han :)

--- datamodel_change.sql
+++ datamodel_change.sql
@@ -1,39 +1,6 @@
 USE zaplog;
-DROP EVENT select_frontpage;
-DELIMITER //
-CREATE EVENT select_frontpage ON SCHEDULE EVERY 90 MINUTE DO
-    BEGIN
-        -- select new frontpage
-        CREATE TABLE frontpage_new
-        SELECT DISTINCT links.* FROM links
-        WHERE published=TRUE
-          AND language=IFNULL((SELECT language FROM channels WHERE id=1),language)
-          AND createdatetime<SUBDATE(CURRENT_TIMESTAMP, INTERVAL 3 HOUR)
-        ORDER BY (score / GREATEST(9, POW(TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, createdatetime),2))) DESC LIMIT 18;
-
-        -- atomic swap
-        RENAME TABLE frontpage_current TO frontpage_old, frontpage_new TO frontpage_current;
-        DROP TABLE frontpage_old;
-
-        -- notify frontpage selection in reactions, use temp table because of triggers
-        CREATE TABLE reactions_temp
-            SELECT
-                id AS linkid,
-                1 AS channelid,
-                "<em>-- selected for frontpage by system --</em>" AS xtext,
-                "-- selected for frontpage by system --" AS description
-            FROM frontpage;
-        INSERT INTO reactions(linkid,channelid,xtext,description)
-            SELECT linkid,channelid,xtext,description FROM reactions_temp;
-        UPDATE reactions AS r
-            JOIN reactions_temp AS t ON r.linkid=t.linkid
-        SET threadid=(SELECT MAX(id) FROM reactions WHERE linkid=r.linkid);
-        DROP TABLE reactions_temp;
-
-        -- notification
-        INSERT INTO interactions(type) VALUES('on_frontpage_calculated');
-    END //
-DELIMITER ;
-SET GLOBAL event_scheduler = ON;
-
+DELETE FROM links WHERE id=29272;
+DELETE FROM links WHERE id=29271;
+DELETE FROM links WHERE id=29270;
+DELETE FROM links WHERE id=29269;
 
