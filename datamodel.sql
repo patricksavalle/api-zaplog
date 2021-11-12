ffdb9061655d80c95f7aba2aac615217f6224a00
Patrick Savalle <patrick@patricksavalle.com>
2021-11-12 12:28:56 +0100

    More robust frontpage algoritm

--- datamodel.sql
+++ datamodel.sql
@@ -259,6 +259,7 @@ DELIMITER //
 CREATE EVENT select_frontpage ON SCHEDULE EVERY 180 MINUTE DO
     BEGIN
         -- select new frontpage
+        DROP TABLE IF EXISTS frontpage_new;
         CREATE TABLE frontpage_new
             SELECT DISTINCT links.* FROM links
             WHERE published=TRUE
@@ -267,23 +268,23 @@ CREATE EVENT select_frontpage ON SCHEDULE EVERY 180 MINUTE DO
             ORDER BY (score / GREATEST(9, POW(TIMESTAMPDIFF(HOUR, CURRENT_TIMESTAMP, createdatetime),2))) DESC LIMIT 18;
 
         -- notify frontpage selection in reactions, use temp table because of triggers
+        DROP TABLE IF EXISTS reactions_temp;
         CREATE TABLE reactions_temp
-            SELECT
-               id AS linkid,
-               1 AS channelid,
-               "<em>-- selected for frontpage by system --</em>" AS xtext,
-               "-- selected for frontpage by system --" AS description
-            FROM frontpage_new WHERE NOT id IN (SELECT id FROM frontpage);
+        SELECT
+            id AS linkid,
+            1 AS channelid,
+            "<em>-- selected for frontpage by system --</em>" AS xtext,
+            "-- selected for frontpage by system --" AS description
+        FROM frontpage_new WHERE NOT id IN (SELECT id FROM frontpage);
         INSERT INTO reactions(linkid,channelid,xtext,description)
-            SELECT linkid,channelid,xtext,description FROM reactions_temp;
+        SELECT linkid,channelid,xtext,description FROM reactions_temp;
         UPDATE reactions AS r
             JOIN reactions_temp AS t ON r.linkid=t.linkid
-            SET threadid=(SELECT MAX(id) FROM reactions WHERE linkid=r.linkid);
-        DROP TABLE reactions_temp;
+        SET threadid=(SELECT MAX(id) FROM reactions WHERE linkid=r.linkid);
 
         -- atomic swap
+        DROP TABLE IF EXISTS frontpage_old;
         RENAME TABLE frontpage_current TO frontpage_old, frontpage_new TO frontpage_current;
-        DROP TABLE frontpage_old;
 
         -- notification
         INSERT INTO interactions(type) VALUES('on_frontpage_calculated');
