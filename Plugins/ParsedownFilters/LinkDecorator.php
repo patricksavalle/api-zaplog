ab4218cf4a62058e2045574d50290512caf2e9aa
Patrick Savalle <patrick@patricksavalle.com>
2021-11-14 23:34:23 +0100

    Inline links in articles are now 'prettyfied' into domains.

--- Plugins/ParsedownFilters/LinkDecorator.php
+++ Plugins/ParsedownFilters/LinkDecorator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Zaplog\Plugins\ParsedownFilters {
 
+    use ContentSyndication\Url;
     use SlimRestApi\Infra\Ini;
     use Zaplog\Plugins\AbstractParsedownFilter;
 
@@ -11,6 +12,11 @@ namespace Zaplog\Plugins\ParsedownFilters {
     {
         function __invoke(array $element): array
         {
+            if (isset($element['attributes']['href'])
+                and isset($element['text'])
+                and strcmp($element['attributes']['href'], $element['text']) === 0) {
+                $element['text'] = (new Url($element['attributes']['href']))->getDomain();
+            }
             static $redirector = null;
             if ($redirector === null) $redirector = Ini::get("broken_link_redirector");
             // add the redirector service to href's
@@ -21,6 +27,7 @@ namespace Zaplog\Plugins\ParsedownFilters {
             if (strcasecmp($element['name'], 'a') === 0) {
                 $element['attributes']['target'] = '_blank';
             }
+
             return $element;
         }
     }
