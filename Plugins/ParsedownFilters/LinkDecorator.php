2ca39e8ba8a3c247d480b47ce1003585aee1df4b
Patrick Savalle <patrick@patricksavalle.com>
2021-11-16 21:39:36 +0100

    Titles in links.

--- Plugins/ParsedownFilters/LinkDecorator.php
+++ Plugins/ParsedownFilters/LinkDecorator.php
@@ -12,22 +12,28 @@ namespace Zaplog\Plugins\ParsedownFilters {
     {
         function __invoke(array $element): array
         {
-            if (isset($element['attributes']['href'])
-                and isset($element['text'])
-                and strcmp($element['attributes']['href'], $element['text']) === 0) {
-                $element['text'] = (new Url($element['attributes']['href']))->getDomain();
-            }
-            static $redirector = null;
-            if ($redirector === null) $redirector = Ini::get("broken_link_redirector");
-            // add the redirector service to href's
-            if (isset($element['attributes']['href'])) {
-                $element['attributes']['href'] = $redirector . $element['attributes']['href'];
-            }
-            // add target= to links
             if (strcasecmp($element['name'], 'a') === 0) {
+
+                // add hover with original url
+                $element['attributes']['title'] = $element['attributes']['href'] ?? "";
+
+                // shorten text of url to domain
+                if (isset($element['attributes']['href'])
+                    and isset($element['text'])
+                    and strcmp($element['attributes']['href'], $element['text']) === 0) {
+                    $element['text'] = (new Url($element['attributes']['href']))->getDomain();
+                }
+
+                // add the redirector service to href's
+                static $redirector = null;
+                if ($redirector === null) $redirector = Ini::get("broken_link_redirector");
+                if (isset($element['attributes']['href'])) {
+                    $element['attributes']['href'] = $redirector . $element['attributes']['href'];
+                }
+
+                // add target= to links
                 $element['attributes']['target'] = '_blank';
             }
-
             return $element;
         }
     }
