889c31793b2624a03f33246fe11199f00eb359cc
Patrick Savalle <patrick@patricksavalle.com>
2021-11-23 10:45:48 +0100

    Safe image embedding and depplinking using normal Markdown notation.

--- Plugins/ParsedownFilters/ImageEmbedder.php
+++ Plugins/ParsedownFilters/ImageEmbedder.php
@@ -8,11 +8,28 @@ namespace Zaplog\Plugins\ParsedownFilters {
     use Exception;
     use Zaplog\Plugins\AbstractParsedownFilter;
 
+    // To avoid copyright claims we must:
+    // - not publish from non-public URL's
+    // - only deep link
+    //
+    // We satisfy these conditions if we only use images from public URL's that
+    // are offered to us by HTML og/twitter metadata.
+
     class ImageEmbedder extends AbstractParsedownFilter
     {
         function __invoke(array $element): array
         {
-            $getImage = function (string $url): ?array {
+            // domain we can safely deeplink
+            $isAllowableDeeplink = function (string $url): bool {
+                foreach ([
+                             "https://pbs.twimg.com/media",
+                         ] as $domain)
+                    if (stripos($url, $domain) === 0) return true;
+                return false;
+            };
+
+            // get deeplinks through safe metadata inspection
+            $getImageByMetadata = function (string $url): ?array {
                 try {
                     // image-specific domains, read image from the public URL HTML-metadata
                     foreach ([
@@ -21,59 +38,76 @@ namespace Zaplog\Plugins\ParsedownFilters {
                                  "https://postimg.cc",
                                  "https://imagebam.com",
                                  "https://www.gettyimages.com",
-                             ] as $domain) {
-                        if (stripos($url, $domain) === 0) {
-                            return ((new HtmlMetadata)($url));
-                        }
-                    }
+                             ] as $domain)
+                        if (stripos($url, $domain) === 0) return (new HtmlMetadata)($url);
                 } catch (Exception $e) {
                     // nothing
                 }
                 return null;
             };
 
-            try {
+            if (strcmp($element['name'], "a") === 0 and isset($element['attributes']['href'])) {
 
-                // Translate <a> elements that are images into <img>
-                //
-                // To avoid copyright claims we must:
-                // - not publish from non-public URL's
-                // - only deep link
-                //
-                // We satisfy these conditions if we only use images from public URL's that
-                // are offered to us by HTML og/twitter metadata.
+                try {
 
-                if (strcmp($element['name'], "a") === 0 and isset($element['attributes']['href'])) {
+                    // for specific domains we will translate links into images
+                    $image = $getImageByMetadata($element['attributes']['href']);
+                    if (!empty($image['image'])) {
 
-                    if (preg_match("@https:\/\/pbs\.twimg\.com\/media\/.*@", $element['attributes']['href']) === 1) {
-                        // https://pbs.twimg.com/media/FEBc-7aUUAAQMxP?format=jpg&name=small
                         return [
                             "name" => "img",
                             "attributes" => [
+                                "class" => "image",
+                                "title" => html_entity_decode($image['title'] ?? ""),
                                 "width" => "100%",
-                                "src" => $element['attributes']['href'],
+                                "src" => $image['image'],
                             ],
                         ];
-
-                    } else {
-
-                        $image = $getImage($element['attributes']['href']);
-                        if (!empty($image['image'])) {
-
-                            return [
-                                "name" => "img",
-                                "attributes" => [
-                                    "title" => html_entity_decode($image['title'] ?? ""),
-                                    "width" => "100%",
-                                    "src" => $image['image'],
-                                ],
-                            ];
-                        }
                     }
+
+                } catch (Exception $e) {
+                    // ignore
                 }
-            } catch (Exception $e) {
-                // ignore
+
+            } elseif (strcmp($element['name'], "img") === 0 and isset($element['attributes']['src'])) {
+
+                try {
+
+                    // Specific domains we can allow as direct deeplinks
+                    if ($isAllowableDeeplink($element['attributes']['src'])) {
+                        return $element;
+                    }
+
+                    // other domains we must get through metadata-inspection
+                    $metadata = (new HtmlMetadata)($element['attributes']['src']);
+                    if (!empty($metadata['image'])) {
+
+                        return [
+                            "name" => "img",
+                            "attributes" => [
+                                "class" => "image",
+                                "title" => html_entity_decode($metadata['title'] ?? ""),
+                                "width" => "100%",
+                                "src" => $metadata['image'],
+                            ],
+                        ];
+                    }
+
+                } catch (Exception $e) {
+                    // ignore
+                }
+
+                // block normal <img> elements to avoid copyright claims, translate into links
+                return [
+                    "name" => "a",
+                    "text" => $element['attributes']['alt'] ?? $element['attributes']['title'],
+                    "attributes" => [
+                        "class" => "blocked-image-link",
+                        "href" => $element['attributes']['src'],
+                    ],
+                ];
             }
+            
             return $element;
         }
     }
