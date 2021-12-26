b5156d6ee9dac6f9fc94297c6cb9b39034e3bac7
Patrick Savalle <patrick@patricksavalle.com>
2021-12-26 16:25:54 +0100

    Refactored translation logic -> separate module.

--- Library/Methods.php
+++ Library/Methods.php
@@ -375,8 +375,8 @@ namespace Zaplog\Library {
                 $system_language = Db::fetch("SELECT language FROM channels WHERE id=1")->language;
                 if (!is_null($system_language) and $link->language !== $system_language /*and Ini::get("auto_translate")*/) {
                     $link->tags = [];
-                    $link->markdown = self::getTranslation($link->markdown, $system_language);
-                    $link->title = self::getTranslation($link->title, $system_language);
+                    $link->markdown = (new Translation)($link->markdown, $system_language);
+                    $link->title = (new Translation)($link->title, $system_language);
                     $link->language = $system_language;
                 }
             }
@@ -431,38 +431,6 @@ namespace Zaplog\Library {
             return $metadata;
         }
 
-        // ----------------------------------------------------------
-        //
-        // ----------------------------------------------------------
-
-        static public function getTranslation(string $text, string $target_lang): string
-        {
-            try {
-                $postdata = http_build_query(
-                    ['auth_key' => Ini::get("deepl_auth_key"),
-                        'target_lang' => $target_lang,
-                        'text' => $text]
-                );
-                $curl = curl_init();
-                curl_setopt($curl, CURLOPT_URL, Ini::get("deepl_api_url"));
-                curl_setopt($curl, CURLOPT_TIMEOUT, 30);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // no echo, just return result
-                curl_setopt($curl, CURLOPT_POST, 1);
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-                $content = curl_exec($curl);
-                $error = curl_error($curl);
-                $errno = curl_errno($curl);
-                if (0 !== $errno or $content === false) {
-                    throw new Exception($error, $errno);
-                }
-            } catch (Exception $e) {
-                error_log($e->getMessage() . " in " . __CLASS__);
-                throw new UserException("Translation service unavailable or failing");
-            } finally {
-                if (is_resource($curl)) curl_close($curl);
-            }
-            return json_decode($content, true)["translations"][0]["text"] ?? $text;
-        }
 
         // ----------------------------------------------------------
         //
@@ -535,6 +503,7 @@ namespace Zaplog\Library {
                 ":copyright" => $link->copyright,
             ];
 
+            /** @noinspection PhpIfWithCommonPartsInspection */
             if (empty($link->id)) {
 
                 (new ServerException)(Db::execute(
---
+++ Library/Translation.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zaplog\Library {
+
+    use Exception;
+    use SlimRestApi\Infra\Ini;
+    use Zaplog\Exception\ServerException;
+
+    class Translation
+    {
+        public function __invoke(string $text, string $target_lang): string
+        {
+            try {
+                $postdata = http_build_query(
+                    ['auth_key' => Ini::get("deepl_auth_key"),
+                        'target_lang' => $target_lang,
+                        'text' => $text]
+                );
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, Ini::get("deepl_api_url"));
+                curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // no echo, just return result
+                curl_setopt($curl, CURLOPT_POST, 1);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
+                $content = curl_exec($curl);
+                $error = curl_error($curl);
+                $errno = curl_errno($curl);
+                if (0 !== $errno or $content === false) {
+                    throw new Exception($error, $errno);
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage() . " in " . __CLASS__);
+                throw new ServerException("Translation service unavailable or failing");
+            } finally {
+                if (is_resource($curl)) curl_close($curl);
+            }
+            return json_decode($content, true)["translations"][0]["text"] ?? $text;
+        }
+    }
+}
