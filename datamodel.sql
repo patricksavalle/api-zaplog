8b02f6ae82e8156321916ed588b4bb34351c7671
Patrick Savalle <patrick@patricksavalle.com>
2022-02-11 15:21:30 +0100

    Tweak.

--- datamodel.sql
+++ datamodel.sql
@@ -669,7 +669,7 @@ CREATE VIEW topreactions AS
             reactions.description,
             reactions.createdatetime
         FROM reactions
-        LEFT JOIN reactionvotes ON reactions.id = reactionid AND reactions.createdatetime > SUBDATE(CURRENT_TIMESTAMP, INTERVAL 12 HOUR)
+        LEFT JOIN reactionvotes ON reactions.id = reactionid AND reactions.createdatetime > SUBDATE(CURRENT_TIMESTAMP, INTERVAL 6 HOUR)
         WHERE reactions.channelid<>1
         GROUP BY reactions.id
         ORDER BY reactions.id DESC LIMIT 50
