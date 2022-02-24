e394ed71cbb548cc7c2f9d1f65a732b5977ab72c
Patrick Savalle <patrick@patricksavalle.com>
2022-02-24 23:15:04 +0100

    links.url field no longer exists.

--- datamodel_change.sql
+++ datamodel_change.sql
@@ -1,10 +1,1 @@
 USE zaplog;
-UPDATE links SET markdown = CONCAT("#", title, "\r\n", IFNULL(markdown,""), IF(url IS NULL,"", CONCAT("\r\n\r\n[", REGEXP_SUBSTR(url, '\\w+\\.\\w+(?=/|$)'), "](", url, ")")));
-ALTER TABLE links
-DROP COLUMN mimetype,
-    DROP COLUMN urlhash,
-    DROP COLUMN url,
-    DROP COLUMN location,
-    DROP COLUMN longitude,
-    DROP COLUMN latitude,
-DROP INDEX urlhash;
